c8731b7af5915041e828dc4cb37ccb9be1dc17c6
Nazim <nazim86@gmail.com>
2023-05-08 21:02:04 +0400

    changed paginatedQuery

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,26 +24,19 @@ export class UserController {
 
   @Get()
   async getUsers(@Query() query: PaginationType) {
-    // const paginatedQuery: PaginationType = getPaginationValues({
-    //   ...query,
-    //   sortBy: `accountData.+${query.sortBy ?? 'createdAt'}`,
-    // });
+    const paginatedQuery: PaginationType = getPaginationValues({
+      ...query,
+      sortBy: `accountData.${query.sortBy ?? 'createdAt'}`,
+    });
 
-    if (query.sortBy) {
-      query.sortBy = `accountData.${query.sortBy}`;
-    } else {
-      query.sortBy = 'accountData.createdAt';
-    }
-    const paginatedQuery: PaginationType = getPaginationValues(query);
-
-    // const paginatedQuery = {
-    //   sortBy: ""query.sortBy ?? 'accountData.createdAt',
-    //   sortDirection: query.sortDirection ?? 'desc',
-    //   pageNumber: isNaN(+query.pageNumber) ? 1 : +query.pageNumber,
-    //   pageSize: isNaN(+query.pageSize) ? 10 : +query.pageSize,
-    //   searchLoginTerm: query.searchLoginTerm ?? null,
-    //   searchEmailTerm: query.searchEmailTerm ?? null,
-    // };
+    // if (query.sortBy) {
+    //   query.sortBy = `accountData.${query.sortBy}`;
+    // } else {
+    //   query.sortBy = 'accountData.createdAt';
+    // }
+    // const paginatedQuery: PaginationType = getPaginationValues(query);
+    //
+    //
 
     const users = await this.userQueryRepo.getUsers(
       paginatedQuery.sortBy,
