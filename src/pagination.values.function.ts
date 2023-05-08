7d8bd8555a90330bf0e1899ff005fd354a09d7da
Nazim <nazim86@gmail.com>
2023-05-08 19:42:19 +0400

    fixed pagination.values.function.ts

--- src/pagination.values.function.ts
+++ src/pagination.values.function.ts
@@ -1,7 +1,7 @@
 export const getPaginationValues = (query: any) => {
   return {
     searchName: query.searchNameTerm ?? '',
-    sortBy: query.sortBy ?? 'createdAt',
+    sortBy: query.sortBy ?? 'accountData.createdAt',
     sortDirection: query.sortDirection ?? 'desc',
     pageNumber: isNaN(+query.pageNumber) ? 1 : +query.pageNumber,
     pageSize: isNaN(+query.pageSize) ? 10 : +query.pageSize,
