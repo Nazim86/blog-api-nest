b21f360eb9120fad19e147ae79c23451d11fd38b
Nazim <nazim86@gmail.com>
2023-08-27 01:05:57 +0400

    getDevices in typeorm

--- src/api/infrastructure/devices/device-query.repo.ts
+++ src/api/infrastructure/devices/device-query.repo.ts
@@ -1,11 +1,16 @@
 import { DeviceViewType } from '../../../types/device-view-type';
 import { Injectable } from '@nestjs/common';
-import { InjectDataSource } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { Devices } from '../../entities/devices/devices.entity';
 
 @Injectable()
 export class DeviceQueryRepo {
-  constructor(@InjectDataSource() private dataSource: DataSource) {}
+  constructor(
+    @InjectDataSource() private dataSource: DataSource,
+    @InjectRepository(Devices)
+    private readonly devicesRepo: Repository<Devices>,
+  ) {}
 
   private deviceMapping = (array, ip: string): DeviceViewType[] => {
     return array.map((device): DeviceViewType => {
@@ -18,14 +23,19 @@ export class DeviceQueryRepo {
     });
   };
   async getDevices(ip: string, userId: string): Promise<DeviceViewType[]> {
-    const device = await this.dataSource.query(
-      `SELECT d.* 
-                    FROM public.devices d
-                    Where d."userId"=$1;`,
-      [userId],
-    );
+    const device = await this.devicesRepo
+      .createQueryBuilder('d')
+      .leftJoin('d.user', 'u')
+      .where('u.id = :userId', { userId: userId })
+      .getMany();
 
-    const mappedDevices: DeviceViewType[] = this.deviceMapping(device, ip);
-    return mappedDevices;
+    // const device = await this.dataSource.query(
+    //   `SELECT d.*
+    //                 FROM public.devices d
+    //                 Where d."userId"=$1;`,
+    //   [userId],
+    // );
+
+    return this.deviceMapping(device, ip);
   }
 }
