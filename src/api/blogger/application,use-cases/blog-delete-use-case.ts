55602ae67a45420f9712deb11796c4d8c06d4f2a
Nazim <nazim86@gmail.com>
2023-09-06 01:54:38 +0400

    commented b.owner in blog-delete-use-case.ts

--- src/api/blogger/application,use-cases/blog-delete-use-case.ts
+++ src/api/blogger/application,use-cases/blog-delete-use-case.ts
@@ -12,15 +12,11 @@ export class BlogDeleteUseCase {
   constructor(private readonly blogRepository: BlogRepository) {}
 
   async execute(command: BlogDeleteCommand): Promise<Result<ResultCode>> {
-    //console.log(command.blogId);
-
     const blog = await this.blogRepository.getBlogById(command.blogId);
-
-    //console.log(blog);
 
     if (!blog) return { code: ResultCode.NotFound };
 
-    if (blog.owner.id !== command.userId) return { code: ResultCode.Forbidden };
+    //if (blog.owner.id !== command.userId) return { code: ResultCode.Forbidden };
 
     const isBlogDeleted = await this.blogRepository.deleteBlogById(
       command.blogId,
